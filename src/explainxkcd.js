b42c92c14b1bbfb6d56092a9aaa84aeb2ad2cf20
Ron Gross <ron.gross@gmail.com>
2012-11-24 17:52:02 +0200

    Delete unused file (thanks Avish)

--- src/explainxkcd.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	debugger;
-	// Make sure we only run on main & comic pages.
-	if (/http:\/\/xkcd\.com\/(\d+\/)?$/.test(window.location.href)) {
-		var xkcdId = document.body.textContent.match(/Permanent link to this comic: http:\/\/xkcd.com\/(\d+)\//)[1];
-	
-		chrome.extension.sendRequest(xkcdId, function(response) {});
-	}
-})();
